402b10f11fc4bcce23f550ede165a3a60e9537f5
cihat <cihat02020202@gmail.com>
2021-04-08 21:54:00 +0300

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/twitter-clone/'
+    : '/',
+  css: {
+    loaderOptions: {
+      sass: {
+          prependData: `
+          @import "@/assets/scss/responsive.scss";
+          `
+      }
+    }
+  }
+};
